37ae7101468bbb642b42924e627c7c3adaa62fe6
ypk <yprasannakumar@gmail.com>
2021-10-04 12:53:22 +0100

    fixed issue with footer added .env for api url

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <footer className="text-gray-600 body-font bg-gray-700">
             <div className="bg-gray-900">
-                <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row text-white text-sm text-center">
+                <div className="container mx-auto py-4 px-5 flex flex-wrap flex-row text-white text-sm text-center">
                     <p className="sm:text-left">&copy;&nbsp;{currentYear},</p>&nbsp;<a className="text-gray-300" href="https://github.com/ypk">ypk</a>.
                     <p className="ml-2">Everything is open-sourced.</p>
                 </div>
